ba82772c58056f98c7e5e7f47a4c8a898cdec731
radox9 <grzywaczra@gmail.com>
2025-05-18 20:18:36 +0200

    test dockera

--- go-api/cmd/server/main.go
+++ go-api/cmd/server/main.go
@@ -9,10 +9,16 @@ import (
 
 func main() {
 	config.LoadEnv()
-	
+
 	r := routes.SetupRoutes()
-	
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("No env PORT")
+	}
+
 	log.Printf("Server is running on port %s", os.Getenv("PORT"))
-	
-	
+	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
+		log.Fatalf("Failed to run server %v", err)
+	}
+
 }
